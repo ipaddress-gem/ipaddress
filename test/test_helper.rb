0a65b5c2b180d01ded12d20ab38e8e8f1f4db672
Francis Luong (Franco) <fluong@fx-mbp-13.local>
2016-03-23 10:23:26 -0400

    add code climate test reporter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,7 @@
 require 'rubygems'
 require 'minitest/autorun'
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
