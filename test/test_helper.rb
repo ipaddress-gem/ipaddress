addce4ce9ce3281fc596485d6f67192923e77250
Alan Somers <asomers@gmail.com>
2017-08-07 17:27:37 -0600

    Update CodeClimate syntax for 1.0.0 compatibility

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'minitest/autorun'
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+require 'simplecov'
+SimpleCov.start
 
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
